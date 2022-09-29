fe755c306ad15a4237d3e7565c440700a5c416b2
Janne Veteläinen <janne.vetelainen@elisanet.fi>
2022-09-29 13:13:32 +0300

    It's not working?

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Fira Code:size=13";
-static int borderpx = 10;
+static int borderpx = 240;
 
 /*
  * What program is execed by st depends of these precedence rules:
